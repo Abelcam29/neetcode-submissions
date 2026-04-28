f0af42d5ec17e0a755a5e28d99ce069a3c61d468
Victor Abel Camacho Rodríguez <144880653+Abelcam29@users.noreply.github.com>
2026-04-27 18:58:33 -0600

    Add: best-time-to-buy-and-sell-stock-ii - submission-2

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() <= 1)
+        {
+            return 0;
+        }
+        int l = 0, r = 1, profit = 0, maxprof = 0;
+        while(r <= (prices.size() - 1))
+        {
+            if(prices[l] <= prices[r])
+            {
+                if(profit >= (prices[r] - prices[l]))
+                {
+                    maxprof += profit;
+                    profit = 0;
+                    l = r;
+                }
+                profit = prices[r] - prices[l];
+            }
+            if(prices[l] > prices[r])
+            {
+                maxprof += profit;
+                profit = 0;
+                l = r;
+            }
+            else
+            {
+                r++;
+            }
+            
+        }
+        maxprof += profit;
+        return maxprof;
+    }
+};
