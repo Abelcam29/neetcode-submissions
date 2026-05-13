c22881039c090665906eb806abfda2ded76e7bb0
Victor Abel Camacho Rodríguez <144880653+Abelcam29@users.noreply.github.com>
2026-05-13 11:46:22 -0600

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> mp;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            mp.insert(nums[i]);
+        }
+        int consecutivem = 0, cons = 0;
+        for(int i : mp)
+        {
+            cons = 1;
+            int x = i+1;
+            while(mp.find(x) != mp.end())
+            {
+                cons++;
+                x++;
+            }
+            if(cons > consecutivem)
+            {
+                consecutivem = cons;
+                cons = 0;
+            }
+        }
+        return consecutivem;
+    }
+};
