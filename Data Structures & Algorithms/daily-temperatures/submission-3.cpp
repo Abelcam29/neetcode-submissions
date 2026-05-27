4ebfa8a2144d8a91b175008cb6b09713ff153e0a
Victor Abel Camacho Rodríguez <144880653+Abelcam29@users.noreply.github.com>
2026-05-27 12:04:20 -0600

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> st;
+        for(int i = temperatures.size() - 1; i >= 0; i--)
+        {
+            st.push(temperatures[i]);
+        }
+        stack<int> temp;
+        st.pop();
+        temp = st;
+        vector<int> res(temperatures.size(), 0);
+        for(int i = 0; i < temperatures.size() - 1; i++)
+        {
+            int sum = 1;
+            while(!temp.empty() && temperatures[i] >= temp.top())
+            {
+                sum++;
+                temp.pop();
+            }
+            if(temp.empty())
+            {
+                sum = 0;
+            }
+            res[i] = sum;
+            st.pop();
+            temp = st;
+            if(st.empty())
+            {
+                break;
+            }
+        }
+
+        return res;
+    }
+};
