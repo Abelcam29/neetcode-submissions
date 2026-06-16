e979440b5f44af9f64ce1668438e13a95278992b
Victor Abel Camacho Rodríguez <144880653+Abelcam29@users.noreply.github.com>
2026-06-16 14:44:34 -0600

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(mp.find(nums[i]) != mp.end())
+            {
+                auto  const it = mp.find(nums[i]);
+                if(abs(i - it->second) <= k)
+                {
+                    return true;
+                }
+            }
+            mp[nums[i]] = i;
+        }
+        return false;
+    }
+};
